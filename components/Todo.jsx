410d8673909168b0d852f48051d85cb4f67d1a64
unknown <teeraphat147258@gmail.com>
2022-08-18 16:11:19 +0700

    update lab

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -7,5 +7,45 @@ import {
 } from "@tabler/icons";
 
 export default function Todo(props) {
-  return <div>your code here...</div>;
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  return (
+    <div
+      onMouseOver={() => {
+        setIsMouseOver(true);
+      }}
+      onMouseOut={() => {
+        setIsMouseOver(false);
+      }}
+    >
+      <div className="border-bottom p-1 py-2 fs-2 d-flex gap-2">
+        <span
+          classname="border-bottom p-1 py-2 fs-2 d-flex gap-2"
+          style={props.completed ? { textDecoration: "line-through" } : null}
+          className="me-auto"
+        >
+          {props.title}
+        </span>
+        {isMouseOver && (
+          <>
+            <button className="btn btn-success" onClick={() => props.onMark()}>
+              <IconCheck />
+            </button>
+            <button className="btn btn-secondary" onClick={() => props.onUp()}>
+              <IconArrowUp />
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => props.onDown()}
+            >
+              <IconArrowDown />
+            </button>
+            <button className="btn btn-danger" onClick={() => props.onDelete()}>
+              <IconTrash />
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
